2113892fae2e44df4fd33d4ffc7695814e355cb4
Joel Bispo <joelbispo04@hotmail.com>
2025-05-02 11:02:34 -0300

    alterações

--- src/server.js
+++ src/server.js
@@ -1,31 +1,32 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
-
 const app = express()
-app.use(bodyParser.json())
+const port = 5000
+
+
+app.use(express.json())
 app.use(cors())
 
+app.post('/analisar', (req, res) => {
+    const mensagem = req.body.msg
 
-app.post('/analisar', (req, res) =>{
-    const  mensagem = req.body.msg
-  
-    if(!mensagem){
+    if (!mensagem) {
         return res.status(400).json('mensagem nao fornecida')
     }
 
     const texto = String(mensagem).toLowerCase()
 
     let setor = 'setor nao indentificado'
-    
-    if(texto.includes('abrir conta') || texto.includes('nova conta')){
+
+    if (texto.includes('abrir conta') || texto.includes('nova conta')) {
         setor = 'Abrir Contas'
-    }else if(texto.includes('cartão') || texto.includes('bloquear cartao')){
+    } else if (texto.includes('cartão') || texto.includes('bloquear cartao')) {
         setor = 'Cartões'
-    }else if(texto.includes('financiamento') || texto.includes(' emprestimo')){
-        setor = ' Credto e Financiamentos'
+    } else if (texto.includes('financiamento') || texto.includes('emprestimo')) {
+        setor = ' Credito e Financiamentos'
     }
-    res.json({ mensagem: `isso é feito no setor ${setor}`})
+    res.json({setor})
+    console.log(setor)
 })
 
-app.listen(5000, () => console.log('servidor rodando'))
+app.listen(port, () => console.log(`servidor rodando na porta ${port}`))
